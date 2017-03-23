1477be774185c01be589c6b0a8c955db3a7e33ae
bqb5176 <bqb5176@110-WL30.up.ist.local>
2017-03-23 18:19:01 -0400

    Start Project

---
+++ CheckersOnline/src/checkersonline/CheckersOnline.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkersonline;
+
+/**
+ *
+ * @author bqb5176
+ */
+public class CheckersOnline {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
